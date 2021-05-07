49e57fb50b871c2d9082e987e601d6189c3ddda1
Ghozti <ybalderas1005@gmail.com>
2021-05-07 13:38:40 -0400

    - added the Listener class

---
+++ src/main/java/Bot/Listener.java
@@ -0,0 +1,13 @@
+package Bot;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+public class Listener implements MessageCreateListener {
+
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+
+    }
+}
---
+++ src/main/java/Bot/Token.java
@@ -0,0 +1,5 @@
+package Bot;
+
+public class Token {
+    public static final String token = "ODM5OTE5MDAyODgwMDQ5MjEy.YJQphw.4aN5zYoyz4kfI8kj7TedpUFODRs";
+}
